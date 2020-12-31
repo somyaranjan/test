384e98538c26bb40e953662512aab6013dc46817
somyaranjan <ranjan.intel@gmail.com>
2020-12-31 14:10:45 +0530

    test 3rd

--- main.py
+++ main.py
@@ -1,11 +1,2 @@
-<<<<<<< HEAD
-=======
-def add():
-    x = 12
-    y = 45
-    i = x+y
-    print(i)
-
-add()
-//test
->>>>>>> 0a08dcd531c23ee121f0699cc3dc564c93ab980e
+my_list = [1,2,3,4,5]
+print(type(my_list))
