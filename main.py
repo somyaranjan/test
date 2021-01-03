ae5620852d5bd7f9b5a7414490f6f85ce7504baa
somyaranjan <ranjan.intel@gmail.com>
2021-01-03 13:54:09 +0530

    TEST

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
-my_list = [1,2,3,4,5]
-print(type(my_list))
+from tkinter import *
+
+#create main window
+main = Tk()
+# main window Title & dimensions
+main.title("welcome screen")
+main.geometry('700x400')
+
+
+#adding widgets
+username_lbl = Label(main, text = "Enter your Username:")
+password_lbl = Label(main, text = "Enter your password:")
+username_lbl.grid()
+password_lbl.grid()
+def clicked():
+    login_btn.configure(text = "done")
+login_btn = Button(main, text = "login", fg = 'red', command = clicked)
+
+login_btn.grid(column = 1, row = 0)
+
+# main loop
+main.mainloop()
